84a448a912458a65b45730a956cde1acefbf8853
breteo <44599374+breteo@users.noreply.github.com>
2023-08-25 16:41:40 -0700

    added js transition

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const form = document.querySelector("[data-js-form]");
+const template = document.querySelector("[data-js-template]");
+const wrapper = document.querySelector("[data-js-wrapper]");
 
-setupCounter(document.querySelector('#counter'))
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const rating = new FormData(event.target).get("rating");
+    if(rating){
+        wrapper.innerHTML = template.innerHTML.replace(/{{rating}}/,rating);
+    }
+    console.log(rating);
+}
+form.addEventListener("submit", handleSubmit);
